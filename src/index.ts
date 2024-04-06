dd23e65b903fa39ad37bbf14d33964d6b288a0c1
wilfreud <killlergangster@gmail.com>
2024-04-06 23:20:56 +0000

    feat: did a lot of things... adding commands

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
 #!/usr/bin/dev node
 
-const hello = (message: string) => {
-  console.log("Hello", message);
-};
+import { Command } from "commander";
 
-hello("there@there");
+// Declare the program
+const program = new Command("quirgo");
+
+// Add options
+program
+  .version(
+    require("../package.json").version,
+    "-v, --version",
+    "Output the current version"
+  )
+  .description(
+    "A simple CLI to manage your GitHub repositories secrets and variables."
+  )
+  .option("-V, --verbose", "Verbose output");
+
+// Add commands
+program
+  .command("vars")
+  .description("Manage repository variables")
+  .argument("<file>", "Path to a .env file to parse")
+  .action((file: string) => {
+    console.log(`Processing ${file}...`);
+  });
+
+program
+  .command("secrets")
+  .description("Manage repository secret")
+  .argument("<set>", "Create a new secret")
+  .argument("<delete>", "Delete a secret");
+
+// Execute CLI with arguments
+program.parse(process.argv);
+
+// Treatment here
+const options = program.opts();
